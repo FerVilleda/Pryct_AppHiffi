db3e32c735ac737cf0866c4076e860c88e15f96e
Maria Fernanda Villeda Dominguez <fer_mcr_95@hotmail.com>
2020-08-18 14:15:19 -0500

    Loggin funcionando con email y pass

--- AppHiffi/src/app/loggin/loggin.page.ts
+++ AppHiffi/src/app/loggin/loggin.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
 
 @Component({
   selector: 'app-loggin',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loggin.page.scss'],
 })
 export class LogginPage implements OnInit {
-
-  constructor() { }
+  user : User = new User();
+  constructor( private router: Router, private authSvc:AuthService) { }
 
   ngOnInit() {
+  }
+
+  async onLogin(){
+    const user = await this.authSvc.onLogin(this.user);
+    
+    if (user) {
+      console.log('Inicio de sesion exitoso');
+      this.router.navigateByUrl('/contenido');
+    }
   }
 
 }
