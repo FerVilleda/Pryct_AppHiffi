a2cf27658ee402742aa10d35abb609d6aba512eb
Maria Fernanda Villeda Dominguez <fer_mcr_95@hotmail.com>
2020-08-18 13:43:46 -0500

    Crear cuenta funcionando

--- AppHiffi/src/app/crear-cuenta/crear-cuenta.page.ts
+++ AppHiffi/src/app/crear-cuenta/crear-cuenta.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
 
 @Component({
   selector: 'app-crear-cuenta',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cuenta.page.scss'],
 })
 export class CrearCuentaPage implements OnInit {
-
-  constructor() { }
+  user: User = new User();
+  constructor( private authSvc: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  //Register
+  async onRegister(){
+    const user = await this.authSvc.onRegister(this.user);
+    if (user) {
+      console.log('Se ha creado el usuario exitosamente');
+      this.router.navigateByUrl('/graciasxunirte');
+    }
   }
 
 }
