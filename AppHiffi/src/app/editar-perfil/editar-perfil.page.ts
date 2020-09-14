2a0fda40924eacebbfcb74dfddc2eb3b9956427d
Maria Fernanda Villeda Dominguez <fer_mcr_95@hotmail.com>
2020-09-14 16:25:57 -0500

    mostrar datos en editar perfil? falta mandalos al input

--- AppHiffi/src/app/editar-perfil/editar-perfil.page.ts
+++ AppHiffi/src/app/editar-perfil/editar-perfil.page.ts
@@ -12,6 +12,9 @@ import { Router } from '@angular/router';
 export class EditarPerfilPage implements OnInit {
 
   usuarioActual : HUsuario;
+  lblUserName: string;
+  lblUserEmpName: string;
+  lblUserPuesto: string;
 
   constructor( private firebaseService: FirebaseService) {
     //Crear usuario vacio
@@ -27,6 +30,22 @@ export class EditarPerfilPage implements OnInit {
      });  
    }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const userData = await this.ObtenerPerfil();
+    this.MostrarDatos(userData);
+  }
+
+  ObtenerPerfil(){
+    return this.firebaseService.getUserProfileData().then(function(resultado){
+    var data: HUsuario;
+    data = resultado;
+    return data;
+    })
+  }
+
+  MostrarDatos(data:HUsuario){
+    this.lblUserName = data.nombre
+    this.lblUserEmpName = data.nombreEmpresa;
+    this.lblUserPuesto = data.puesto;
   }
 }
