01912bcd8c4bd0ce234985405c2b7ca4a520ebe4
LilianaDJ <isc-lili@outlook.com>
2020-09-22 21:42:16 -0500

    Modificacion de pantalla "Grafica"

--- AppHiffi/src/app/graficos-xuser/graficos-xuser.page.ts
+++ AppHiffi/src/app/graficos-xuser/graficos-xuser.page.ts
@@ -33,7 +33,7 @@ export class GraficosXuserPage implements OnInit {
     const act = await this.obtenRespuestas();
     console.log(act);
     this.createBarChart(act.Respuestas);
-    this.createGenerateChart();
+    //this.createGenerateChart();
   }
 
 
@@ -68,35 +68,35 @@ export class GraficosXuserPage implements OnInit {
     });
   }
 
-  createGenerateChart() {
-    this.bars = new Chart(this.generateChart.nativeElement,{
-      type: 'line',
-      data: {
-        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Your responses',
-          data: [4, 3, 3, 4, 5, 3, 2, 4],
-          backgroundColor: 'rgba(0,0,0,0)', // array should have same number of elements as number of dataset
-          borderColor: 'rgb(39, 69, 204)',// array should have same number of elements as number of dataset
-          borderWidth: 2
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            gridLines:{
-              drawBorder: false,
-              color: ['limegreen', 'green', 'yellow', 'pink', 'red']
-            },
-            ticks: {
-              min: 0,
-              max: 5,
-              stepSize: 1  
-            }
-          }]
-        }
-      }
-    });
-  }
+  // createGenerateChart() {
+    //this.bars = new Chart(this.generateChart.nativeElement,{
+      //type: 'line',
+      //data: {
+        //labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        //datasets: [{
+          //label: 'Your responses',
+          //data: [4, 3, 3, 4, 5, 3, 2, 4],
+          //backgroundColor: 'rgba(0,0,0,0)', // array should have same number of elements as number of dataset
+          //borderColor: 'rgb(39, 69, 204)',// array should have same number of elements as number of dataset
+          //borderWidth: 2
+       // }]
+      //},
+      //options: {
+        //scales: {
+          //yAxes: [{
+            //gridLines:{
+              //drawBorder: false,
+              //color: ['limegreen', 'green', 'yellow', 'pink', 'red']
+            //},
+            //ticks: {
+             // min: 0,
+             // max: 5,
+             // stepSize: 1  
+          //  }
+          //}]
+       // }
+      //}
+    //});
+  //}
 
 }
