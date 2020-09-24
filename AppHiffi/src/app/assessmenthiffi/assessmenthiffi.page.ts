31f65886d2a95d4c97dbffcfcfc15922ff138b9b
Maria Fernanda Villeda Dominguez <fernanda.villeda@duxdiligens.onmicrosoft.com>
2020-09-24 12:44:31 -0500

    Actualizacion de respuesta al darle Anterior, Ok y funcionando. Assessment completo.

--- AppHiffi/src/app/assessmenthiffi/assessmenthiffi.page.ts
+++ AppHiffi/src/app/assessmenthiffi/assessmenthiffi.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service'
 import { Assessment } from '../interfaces/assessment';
-import { IonSlides } from '@ionic/angular';
+import { IonSlides, NumericValueAccessor } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,6 +16,8 @@ export class AssessmenthiffiPage implements OnInit {
   res: number;
   Opciones;
   resActual: Assessment;
+  r1; r2; r3; r4; r5; r6; r7;
+  
  
   buttonName: string;
   disablePrevBtn = false;
@@ -35,7 +37,8 @@ export class AssessmenthiffiPage implements OnInit {
   async Next(){
     const idx = await this.slideChanged(this.e);
     if (idx == 6) {
-      this.respuestas[idx] = this.res;
+      this.respuestas.push(this.r1,this.r2,this.r3,this.r4,this.r5,this.r6,this.r7);
+      console.log(this.respuestas);
       this.resActual.Respuestas = this.respuestas;
       this.firebaseService.insertarRespuestas(this.resActual).then(() => {
         console.log('Respuestas guardadas correctamente');
@@ -43,10 +46,7 @@ export class AssessmenthiffiPage implements OnInit {
         console.error(error);
       });
       this.router.navigateByUrl("/resultadoshiffi");     
-    } else {
-      this.respuestas[idx]=this.res;
-      this.resActual.Respuestas = this.respuestas;
-      console.log(this.respuestas);
+    } else {     
       this.slides.slideNext();
     }
   }
@@ -56,10 +56,34 @@ export class AssessmenthiffiPage implements OnInit {
     this.slides.slidePrev();
   }
   
-  checkValue(event){
-    console.log(event.detail.value);
-    this.res = Number(event.detail.value);
-    return this.res;
+  async checkValue(event){
+
+    const idx = await this.slideChanged(this.e);
+    switch (idx) {
+      case 0:
+        this.r1 = Number(event.detail.value);
+        break;
+      case 1:
+        this.r2 = Number(event.detail.value);
+        break;
+      case 2:
+        this.r3 = Number(event.detail.value);
+        break;
+      case 3:
+        this.r4 = Number(event.detail.value);
+        break;
+      case 4:
+        this.r5 = Number(event.detail.value);
+        break;
+      case 5:
+          this.r6 = Number(event.detail.value);
+          break;
+      case 6:
+        this.r7 = Number(event.detail.value);
+        break;
+      default:
+        break;
+    }
   }
 
 
@@ -81,14 +105,4 @@ export class AssessmenthiffiPage implements OnInit {
     this.disablePrevBtn = true;
   }
 
-  ObtenerRespuesta(event){
-    var vaor = event;
-    console.log("ver valor siii selecciono el 1 de la primera respuesta");
-    console.log(vaor);
-  }
-
-
-
-
-  
 }
