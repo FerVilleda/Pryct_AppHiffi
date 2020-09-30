2749b0262bfb0d9bff1b417afa221172422cce23
FerVilleda <fer_mcr_95@hotmail.com>
2020-09-30 14:43:59 -0500

    ioncards en contenido para mejorar apariencia

--- AppHiffi/src/app/contenido/contenido.page.ts
+++ AppHiffi/src/app/contenido/contenido.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-contenido',
@@ -8,7 +9,7 @@ import { MenuController } from '@ionic/angular';
 })
 export class ContenidoPage implements OnInit {
 
-  constructor( private menuCtrl: MenuController) { }
+  constructor( private menuCtrl: MenuController, private iab: InAppBrowser) { }
 
   ngOnInit() {
   }
@@ -19,6 +20,14 @@ export class ContenidoPage implements OnInit {
 
   toggleMenu(){
     this.menuCtrl.toggle("primerMenu");
+  }
+
+  openSystem(){
+    const browser = this.iab.create('https://hiffi-group-academy.thinkific.com/courses/holistic-strategic-innovation','_system');
+  }
+
+  openFramework(){
+    const browser = this.iab.create('https://www.hiffigroup.org/framework/','_system');
   }
 
 }
